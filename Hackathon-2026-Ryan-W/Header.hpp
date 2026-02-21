10c3028b66bc6e936b0fc2a779860e2276dd160f
Ryan <ryangarretwilson872@gmail.com>
2026-02-21 12:51:06 -0800

    Trying to get SFML working

--- Hackathon-2026-Ryan-W/Header.hpp
+++ Hackathon-2026-Ryan-W/Header.hpp
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream> // cout, endl
+
+using namespace std;
---
+++ Hackathon-2026-Ryan-W/ProgramWindow.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+/*
+	Summary: Window interface for the program. Idk what I'm making yet.
+
+
+*/
+
+#include "Header.hpp"
+
+class ProgramWindow {
+public:
+	// structors
+	ProgramWindow() {}
+	~ProgramWindow() {}
+
+private:
+
+
+
+};
